f637df5651fe463243b9781af295ac7c2bcefc50
Nisha <its.nish.0709@gmail.com>
2025-04-30 11:24:35 +0530

    V1: Project setup with User and ContactDetails classes, and sample users.json

---
+++ Models/ContactDetails.cs
@@ -0,0 +1,8 @@
+namespace UserDataProject.Models
+{
+    public class ContactDetails
+    {
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace UserDataProject.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Nationality { get; set; }
+        public ContactDetails Contact { get; set; }
+    }
+}
